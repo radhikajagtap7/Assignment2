cf50e3110ef4bbf7877e48b249ef783c5590a2b8
mukeshvchavan <mukeshchavan2504@gmail.com>
2023-07-29 21:46:23 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    Stack s1 = new Stack();
+    s1.push(11);
+    s1.push(22);
+    s1.push(33);
+
+    System.out.println(s1.peek());
+
+    s1.push(44);
+    s1.push(55);
+
+    System.out.println(s1.pop());
+
+    s1.push(66);
+    s1.push(77);
+    s1.push(88);
+
+
+
+    s1.show();
+
+        }
+    }
---
+++ src/Stack.java
@@ -0,0 +1,42 @@
+public class Stack {
+    int stack[] = new int[6];
+    int top = 0;
+    public void push(int data){
+        if (top == 6){
+            System.out.println("Stack is Full");
+        }
+        else  {
+            stack[top] = data;
+            top++;
+        }
+        }
+
+        public int pop() {
+            int data = 0;
+            if (isEmpty()) {
+                System.out.println("Stack is Empty");
+            } else {
+                top--;
+                data = stack[top];
+                stack[top] = 0;
+            }
+            return data;
+        }
+
+        public int peek(){
+        int data;
+        data = stack[top - 1];
+        return data;
+        }
+
+        public boolean isEmpty(){
+        return  top <= 0;
+        }
+
+        public void show(){
+        for (int n : stack){
+            System.out.println(n + " ");
+        }
+        }
+
+}
